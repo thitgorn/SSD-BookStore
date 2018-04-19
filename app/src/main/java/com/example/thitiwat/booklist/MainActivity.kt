1c22d79d5689c38c466172da3aaa4a3e05af333a
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2018-04-19 16:57:42 +0700

    update

--- app/src/main/java/com/example/thitiwat/booklist/MainActivity.kt
+++ app/src/main/java/com/example/thitiwat/booklist/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.thitiwat.booklist
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.Menu
+import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.SearchView
 import com.example.thitiwat.booklist.model.Book
 import com.example.thitiwat.booklist.model.DataBookRepository
-import com.example.thitiwat.booklist.model.MockBookRepository
 import com.example.thitiwat.booklist.presenter.BookPresenter
 import com.example.thitiwat.booklist.presenter.BookView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -26,18 +29,24 @@ class MainActivity : AppCompatActivity(), BookView {
 
     }
 
+    fun startUserActivity(view : View) {
+        val intent = Intent(this, UserActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, "SEND MESSAGE")
+        }
+        startActivity(intent)
+    }
+
     override fun setBookList(books: ArrayList<Book>) {
         val array = arrayOfNulls<String>(books.size)
 
         for (i in 0..(books.size-1)) {
             array[i] = books.get(i).toString()
         }
-//
         var adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,array)
         list_view.adapter = adapter
     }
 
-    fun syncSearchView(searchView: SearchView) {
+    private fun syncSearchView(searchView: SearchView) {
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
 
             override fun onQueryTextChange(newText: String): Boolean {
---
+++ app/src/main/java/com/example/thitiwat/booklist/UserActivity.kt
@@ -0,0 +1,12 @@
+package com.example.thitiwat.booklist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class UserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user)
+    }
+}
